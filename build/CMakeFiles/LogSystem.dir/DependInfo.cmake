19bd3befe34d529f3cf219802c0eb4411f993d3f
Fisher-y <2929904399@qq.com>
2025-09-16 20:13:50 +0800

    cmake编译提交

---
+++ build/CMakeFiles/LogSystem.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/STUDY/C++/VScode/log_system2/Logger.cpp" "/mnt/d/STUDY/C++/VScode/log_system2/build/CMakeFiles/LogSystem.dir/Logger.cpp.o"
+  "/mnt/d/STUDY/C++/VScode/log_system2/TaskManager.cpp" "/mnt/d/STUDY/C++/VScode/log_system2/build/CMakeFiles/LogSystem.dir/TaskManager.cpp.o"
+  "/mnt/d/STUDY/C++/VScode/log_system2/main.cpp" "/mnt/d/STUDY/C++/VScode/log_system2/build/CMakeFiles/LogSystem.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/LogSystem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LogSystem.dir/Logger.cpp.o"
+  "CMakeFiles/LogSystem.dir/TaskManager.cpp.o"
+  "CMakeFiles/LogSystem.dir/main.cpp.o"
+  "LogSystem"
+  "LogSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LogSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
